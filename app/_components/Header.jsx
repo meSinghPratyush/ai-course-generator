25e4a874f2e13b6831d9886f28676611c4fbcc1c
meSinghPratyush <pratyush1555@gmail.com>
2026-02-19 13:26:43 +0530

    Testing branch initial commit

---
+++ app/_components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from "next/image";
+import { Button } from "@/components/ui/button";    
+
+function Header() {
+  return (
+    <div className='flex justify-between p-4'>
+        <Image src="/logo.svg" alt="Logo" width={150} height={100} />
+        <Button>Get Started</Button>
+    </div>
+  )
+}
+
+export default Header
--- configs/db.jsx
+++ configs/db.jsx
@@ -1,7 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-const sql = neon(process.env.NEXT_PUBLIC_DB_CONNECTION_STRING);
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
 export const db = drizzle({ client: sql });
 
 
